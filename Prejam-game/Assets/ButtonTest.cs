61002576b05bd437749568402e8a84b09d59218a
Shagar <sylvain.gaborit@depinfonancy.net>
2021-07-29 10:09:41 +0200

    First test: random title

---
+++ Prejam-game/Assets/ButtonTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour
+{
+    public Text titleText;
+    public string[] Titles;
+    public Button yourButton;
+
+    void Start()
+    {
+        //Button btn = yourButton.GetComponent<Button>();
+        //btn.onClick.AddListener(RandomTitle);
+        yourButton.onClick.AddListener(RandomTitle);
+    }
+
+
+    public void RandomTitle()
+    {
+        titleText.text = Titles[Random.Range(0, Titles.Length)];
+        titleText.color = Random.ColorHSV();
+    }
+
+    
+}

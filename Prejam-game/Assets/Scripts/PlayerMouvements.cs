1ef2f1d5176007645df5bac7d5813fd34e4382a5
Shagar <sylvain.gaborit@depinfonancy.net>
2021-07-29 14:06:55 +0200

    First test scene and mouvement script.

---
+++ Prejam-game/Assets/Scripts/PlayerMouvements.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouvements : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        // If the input is moving the player right and the player is facing left...
+        if (movement.x > 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (movement.x < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
